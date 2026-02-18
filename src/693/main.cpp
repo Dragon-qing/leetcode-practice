d65d92250158e5950fe764543a29f2231f30f364
Dragon-qing <1259337898@qq.com>
2026-02-18 10:04:40 +0800

    feat: 693题解

---
+++ src/693/main.cpp
@@ -0,0 +1,33 @@
+#include <iostream>
+#include <bitset>
+#include <string.h>
+using namespace std;
+
+class Solution {
+public:
+    bool hasAlternatingBits(int n) {
+        bool falg = true;
+        char tmp = '\0';
+        string s = bitset<32>(n).to_string();
+        s = s.substr(s.find('1')); // 去除前置的0
+        
+        for(char c: s)
+        {
+            if (c == tmp)
+            {
+                falg = false;
+                break;
+            }
+            tmp = c;
+        }
+        
+        return falg;
+    }
+};
+
+int main()
+{
+    Solution s;
+    std::cout << s.hasAlternatingBits(4);
+    return 0;
+}

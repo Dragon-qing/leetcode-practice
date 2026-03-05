6d0835ced5c3f0e145f922c8370e8bfc65bed247
dragon-qing <1259337898@qq.com>
2026-03-06 03:09:26 +0800

    feat: 18---双指针，四数相加（代码随想录） https://leetcode.cn/problems/4sum

---
+++ src/18/main.cpp
@@ -0,0 +1,63 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+class Solution {
+public:
+    vector<vector<int>> fourSum(vector<int>& nums, int target) {
+        vector<vector<int>> ans{};
+        int n = nums.size();
+        int left = 0, right = n - 1;
+        ranges::sort(nums);
+        for (int i = 0; i < n; i++)
+        {
+            if (nums[i] > target && nums[i] >= 0)
+            {
+                break;
+            }
+            if (i > 0 && nums[i] == nums[i - 1])
+            {
+                continue;
+            }
+            for (int j = i + 1; j < n; j++)
+            {
+                if (nums[j] + nums[i] > target && nums[j] >= 0)
+                {
+                    break;
+                }
+                if (j > i + 1 && nums[j] == nums[j - 1])
+                {
+                    continue;
+                }
+                left = j + 1;
+                right = n - 1;
+                while (left < right)
+                {
+                    if ((long)nums[i] + nums[j] + nums[left] + nums[right] < target)
+                    {
+                        left++;
+                    }
+                    else if ((long)nums[i] + nums[j] + nums[left] + nums[right] > target)
+                    {
+                        right--;
+                    }
+                    else
+                    {
+                        ans.emplace_back(vector<int>{nums[i], nums[j], nums[left], nums[right]});
+                        while (left < right && nums[left + 1] == nums[left]){ left++; }
+                        while (left < right && nums[right - 1] == nums[right]){ right--; }
+                        left++;
+                        right--;
+                    }
+                }
+            }
+        }
+        return ans;
+    }
+};
+
+int main()
+{
+    Solution s;
+
+    return 0;
+}

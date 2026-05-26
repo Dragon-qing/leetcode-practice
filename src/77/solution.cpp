6ef7ac59c2bbec9dfaa1e4663dd28002e4795585
Dragon-qing <1259337898@qq.com>
2026-05-26 21:57:41 +0800

    feat: 77 - 组合，回溯

---
+++ src/77/solution.cpp
@@ -0,0 +1,57 @@
+/*
+ * @Author: Dragon-qing
+ * @Date: 2026-05-26
+ * @LastEditors: Dragon-qing
+ * @FilePath: \leetcode\src\77\solution.cpp
+ * @Description: 回朔
+ */
+#include <bits/stdc++.h>
+#include "fmt/base.h"
+#include "fmt/ranges.h"
+using namespace std;
+
+/*
+ * @lc app=leetcode.cn id=77 lang=cpp
+ *
+ * [77] 组合
+ */
+
+// @lc code=start
+class Solution {
+public:
+    void BackTracking(int startidx, int n, int k)
+    {
+        if (cur.size() == k)
+        {
+            res.push_back(cur);
+            return;
+        }
+
+        for (int i = startidx; i <= n - (k - cur.size()) + 1; i++)
+        {
+            cur.push_back(i);
+            BackTracking(i + 1, n, k);
+            cur.pop_back(); // 回溯
+        }
+    }
+    vector<vector<int>> combine(int n, int k) {
+        BackTracking(1, n, k);
+        return res;
+    }
+private:
+    vector<int> cur;
+    vector<vector<int>> res;
+};
+
+int main()
+{
+    Solution s;
+    auto res = s.combine(4, 4);
+    for (auto v : res) {
+        fmt::println("{}", v);
+    }
+    return 0;
+}
+
+// @lc code=end
+
